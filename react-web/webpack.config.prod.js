70c273b927a1bb50673f3f534ed129af79c6f28e
ada <3532371088@qq.com>
2021-08-02 22:06:07 +0800

    init

---
+++ react-web/webpack.config.prod.js
@@ -0,0 +1,144 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HappyPack = require('happypack');;
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const theme = require('./src/config/theme');
+const os = require('os');
+const happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length });
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'app.ts'),
+  output: {
+    filename: 'sites-pc.js',
+    path: path.resolve(__dirname, 'dist'),
+    chunkFilename: 'simple-os.[name].async.js',
+  },
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/'),
+    },
+    extensions: ['.js', '.jsx', '.css', '.less', '.ts', '.tsx'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js|jsx$/,
+        include: [path.resolve(__dirname, 'src')],
+        use: ['happypack/loader?id=babel'],
+      },
+      {
+        test: /\.ts|tsx$/,
+        loader: ['happypack/loader?id=babel', 'awesome-typescript-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: {
+                localIdentName: '[name]_[local]-[hash:base64:5]',
+              },
+            },
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              javascriptEnabled: true,
+              modifyVars: theme,
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              javascriptEnabled: true,
+              modifyVars: theme,
+            },
+          },
+        ],
+        exclude: /src/,
+      },
+    ],
+  },
+  stats: {
+    children: false,
+    warningsFilter: (warn) => warn.indexOf('Conflicting order between:') > -1,
+  },
+  node: {
+    fs: 'empty',
+    module: 'empty',
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.(css|less)/,
+          chunks: 'all',
+          enforce: true,
+        },
+        commons: {
+          name: 'commons',
+          chunks: 'initial',
+          minChunks: 2,
+        },
+        vendors: {
+          name: 'vendors',
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+      },
+    },
+    runtimeChunk: true,
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorOptions: { discardComments: { removeAll: true } },
+      canPrint: true,
+    }),
+    new HappyPack({
+      id: 'babel',
+      loaders: ['babel-loader?cacheDirectory'],
+      threadPool: happyThreadPool,
+    }),
+  ],
+};
