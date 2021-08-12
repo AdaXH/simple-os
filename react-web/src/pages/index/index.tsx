e2cef96a8a5f3bc01e076becc93a8d5bdc5b3b39
linghua <linghua@mogu.com>
2021-08-12 18:47:14 +0800

    init

--- react-web/src/pages/index/index.tsx
+++ react-web/src/pages/index/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Loading from '@/component/loadingWraper';
+import Galaxy from '@/component/galaxy';
 import { UserInfo } from './component/userInfo';
 
 export default ({ history }) => (
   <>
     <Loading />
-    <UserInfo history={history}/>
+    {/* <UserInfo history={history}/> */}
+    <Galaxy />
   </>
 );
